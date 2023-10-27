975298a0c5c1661b9e2ec3f42d776a38db5769ba
bober2005 <yaroslavd1290@gmail.com>
2023-10-27 14:16:19 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { ToTop } from './components/ScrollTpTop/ToTop';
+import { theme } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
+      <ThemeProvider theme={theme}>
+        <App />
+        <ToTop />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
